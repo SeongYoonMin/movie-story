52ab5746e44258bd0e8dcb25ddf30798a0adc0a4
SeongYoonMin <namo4862@naver.com>
2023-07-31 23:07:47 +0900

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  typescript: {
+    shim: false,
+  },
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      movie_url: "https://api.themoviedb.org/3",
+      access_token:
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTYyNjM5MTMzZTE2ODlhN2VjZTMxYzQxNDBmMTYwNyIsInN1YiI6IjY0ODFjZjc4ZTI3MjYwMDE0N2I4ODhmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rNGh_Ij7dWQ1We97kKo_oymFHNQBsOLw1Pei8mX6338bb",
+      api_key: "0962639133e1689a7ece31c4140f1607",
+    },
+  },
+  app: {
+    head: {
+      title: "MOVIE TIME",
+    },
+  },
+  modules: ["@pinia/nuxt"],
+});
---
+++ type/index.d.ts
@@ -0,0 +1,31 @@
+interface IConfig {
+  public: {
+    movie_url: string;
+    access_token: string;
+    api_key: string;
+  };
+}
+
+interface IMovieItems {
+  adault: boolean;
+  backdrop_path: string;
+  genre_ids: any;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface IMovieList {
+  page: number;
+  results: IMovieItems[];
+  total_pages: number;
+  total_results: number;
+}
