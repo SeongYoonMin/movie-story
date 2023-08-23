b6dcd74ae7ec55d3d8d50b61c22dd310d758ee37
SeongYoonMin <namo4862@naver.com>
2023-08-23 23:15:24 +0900

    ADD: SearchList Components

---
+++ components/section/SearchList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface ISearchProps {
+  results: IMovieItems[];
+}
+const props = defineProps<ISearchProps>();
+</script>
+
+<template>
+  <section v-if="props.results" class="search_list">
+    <div v-for="items in props.results" class="items">
+      <picture class="thumb">
+        <img v-if="items.poster_path === null" src="~/assets/images/null_image.png" alt="이미지 찾기 실패">
+        <img v-else :src="'https://image.tmdb.org/t/p/w500/' + items.poster_path" :alt="items.title">
+      </picture>
+
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.search_list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 12px;
+  column-gap: 40px;
+  .items {
+    .thumb {
+      display: block;
+      width: 100%;
+      img {
+        max-width: 100%;
+        border-radius: 8px;
+      }
+    }
+  }
+}
+</style>
