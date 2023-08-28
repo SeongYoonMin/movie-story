444a96e47a74be25da6f4150097388883a0b1b19
SeongYoonMin <namo4862@naver.com>
2023-08-28 23:11:51 +0900

    Fix: add ISearchItems, IDramaDetail

--- type/index.d.ts
+++ type/index.d.ts
@@ -33,7 +33,9 @@ interface IMovieItems {
   vote_count: number;
   origin_country: string[];
 }
-
+interface ISearchItems extends IMovieItems {
+  first_air_date: string;
+}
 interface IMovieList {
   page: number;
   results: IMovieItems[];
@@ -53,6 +55,7 @@ interface ISearchQuery {
   page: number;
   region: string;
   include_adult: true | false;
+  first_air_date_year: string;
   primary_release_year: string;
   year: string;
 }
@@ -95,6 +98,98 @@ interface IMovieDetail {
   tagline: string;
   title: string;
   video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface IDramaDetail {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: any[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    air_date: string;
+    episode_number: number;
+    episode_type: string;
+    production_code?: any;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path?: number;
+  };
+  name: string;
+  next_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    air_date: string;
+    episode_number: number;
+    episode_type: string;
+    production_code?: any;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path?: number;
+  };
+  networks: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: nuberm;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+    vote_average: number;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  type: string;
   vote_average: number;
   vote_count: number;
 }
